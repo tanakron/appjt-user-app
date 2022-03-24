f7b778117b653b1fed3206bf9c4df86740006800
tanakorn <up2com1988@gmail.com>
2022-03-24 09:06:57 +0700

    add
    add

---
+++ src/views/pages/account/dashboardsusers.vue
@@ -0,0 +1,40 @@
+<script>
+export default {};
+</script>
+
+<style></style>
+
+<template>
+  <Layout>
+    <PageHeader :title="title" :items="items" />
+    <div class="row">
+      <div class="col-xl-8">
+        <Stat />
+        <RevenueAnalytics />
+      </div>
+      <div class="col-xl-4">
+        <SalesAnalytics />
+        <EarningReport />
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-lg-4">
+        <Sources />
+      </div>
+      <div class="col-lg-4">
+        <RecentActivity />
+      </div>
+      <div class="col-lg-4">
+        <RevenueLocation />
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-lg-4">
+        <Chat />
+      </div>
+      <div class="col-lg-8">
+        <Transaction />
+      </div>
+    </div>
+  </Layout>
+</template>
